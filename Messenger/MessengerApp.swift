8dd660c3d47d0d396fad77868565450c4fb75202
Ankitrajput0001 <ankitrajput44408@gmail.com>
2024-07-10 17:28:14 +0530

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Ankit Rajput on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
